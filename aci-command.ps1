6d85e81049412298b60c44a974cb668698e19671
klacol <klaus.aengenvoort@etask.de>
2021-11-24 06:27:39 +0100

    Parameters

--- aci-command.ps1
+++ aci-command.ps1
@@ -1,14 +1,16 @@
 param (
     [Parameter(Mandatory = $false)]$containerName = $env:containerName,
     [Parameter(Mandatory = $false)]$command = $env:command,
-    [Parameter(Mandatory = $false)]$resourceGroup = $env:resourceGroup,
     [Parameter(Mandatory = $false)]$tenant = $env:tenant,
-    [Parameter(Mandatory = $false)]$username = $env:username ,
+    [Parameter(Mandatory = $false)]$resourceGroup = $env:resourceGroup,
+    [Parameter(Mandatory = $false)]$username = $env:username,
     [Parameter(Mandatory = $false)]$password = $env:password
 )
 az login --service-principal -u $username -p $password --tenant $tenant
 Write-Host "-----Logged In -----"
 
 $command = $command.ToLower()
+Write-Host "command:"$command
+Write-Host "containerName:"$containerName
 
 az container $command --name $containerName --resource-group $resourceGroup
